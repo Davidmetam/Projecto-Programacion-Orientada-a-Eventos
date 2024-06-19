e6384a8e2834dc7230bf81163ea499e8467e2d07
David <david.mendoza.tam@gmail.com>
2024-06-18 23:03:47 -0600

    Pagina principal implementacion

--- src/main/java/escolar/poe/PaginaPrincipal.java
+++ src/main/java/escolar/poe/PaginaPrincipal.java
@@ -1,6 +1,12 @@
 package escolar.poe;
 
+import escolar.poe.services.Materia;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PaginaPrincipal {
     private JPanel panel1;
@@ -13,16 +19,58 @@ public class PaginaPrincipal {
     private JButton registrarMateriaButton;
     private JComboBox seleccionarMateriasBox;
     private String userType;
+    List<Materia> materias;
+    DefaultComboBoxModel<String> model;
 
     public PaginaPrincipal(String userType) {
+        materias = new ArrayList<>();
         this.userType = userType;
+        seleccionarMateriasBox.addItem("");
+        if (!materias.isEmpty()) {
+            for (Materia materia : materias) {
+                seleccionarMateriasBox.addItem(materia.getNombre());
+            }
+        }
+
         if (this.userType.equals("docente")) {
             comenzarPruebaButton.setText("Ver Reportes");
             seleccionarMateriasBox.setVisible(false);
         } else if (this.userType.equals("alumno")) {
             registrarMateriaButton.setVisible(false);
         }
+        comenzarPruebaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        verMisRespuestasButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        registrarMateriaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String materia = JOptionPane.showInputDialog("Ingrese el nombre de la materia");
+                if (materia.isEmpty()) {
+                    return;
+                }
+                materias.add(new Materia(materia));
+                JOptionPane.showMessageDialog(null, "Materia agregada");
+            }
+        });
     }
+
+    public List<Materia> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(List<Materia> materias) {
+        this.materias = materias;
+    }
+
     public JPanel getPrincipalGUI() {
         return PrincipalGUI;
     }
