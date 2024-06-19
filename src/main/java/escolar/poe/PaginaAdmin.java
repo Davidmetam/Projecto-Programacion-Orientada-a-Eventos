e3526e2f460c007d7ddc18593dcdad0d198ae092
David <david.mendoza.tam@gmail.com>
2024-06-19 00:14:47 -0600

    Pagina principal implementacion

--- src/main/java/escolar/poe/PaginaAdmin.java
+++ src/main/java/escolar/poe/PaginaAdmin.java
@@ -47,6 +47,7 @@ public class PaginaAdmin {
             }
         });
         registrarButton.addActionListener(new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
 
