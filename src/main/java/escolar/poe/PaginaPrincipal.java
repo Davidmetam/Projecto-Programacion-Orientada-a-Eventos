51fa3fc0759cc62531b9dff20efa86b7c0fcc1a5
David <david.mendoza.tam@gmail.com>
2024-06-18 22:31:44 -0600

    Pagina principal implementacion

--- src/main/java/escolar/poe/PaginaPrincipal.java
+++ src/main/java/escolar/poe/PaginaPrincipal.java
@@ -10,12 +10,19 @@ public class PaginaPrincipal {
     private JButton verMisRespuestasButton;
     private JButton comenzarPruebaButton;
     private JPanel PrincipalGUI;
+    private JButton registrarMateriaButton;
+    private JComboBox seleccionarMateriasBox;
     private String userType;
 
     public PaginaPrincipal(String userType) {
         this.userType = userType;
+        if (this.userType.equals("docente")) {
+            comenzarPruebaButton.setText("Ver Reportes");
+            seleccionarMateriasBox.setVisible(false);
+        } else if (this.userType.equals("alumno")) {
+            registrarMateriaButton.setVisible(false);
+        }
     }
-
     public JPanel getPrincipalGUI() {
         return PrincipalGUI;
     }
