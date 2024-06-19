d9009bdd409145fb438a1c45a3347907a440ec78
DESKTOP-4K6ICRT\uncle <moes131212@gmail.com>
2024-06-18 21:40:15 -0600

    Elemntos listos del resto de Vistas

---
+++ src/main/java/escolar/poe/ReporteResultados.java
@@ -0,0 +1,10 @@
+package escolar.poe;
+
+import javax.swing.*;
+
+public class ReporteResultados {
+    private JPanel ReportesGUI;
+    private JButton volverButton;
+    private JTextArea MensajeDocente;
+    private JTextArea ReporteResultados;
+}
---
+++ src/main/java/escolar/poe/RespuestasAlumno.java
@@ -0,0 +1,10 @@
+package escolar.poe;
+
+import javax.swing.*;
+
+public class RespuestasAlumno {
+    private JPanel ResAlumnoGUI;
+    private JTextArea RespuestasAlumno;
+    private JTextArea MensajeAlumno;
+    private JButton volverButton;
+}
